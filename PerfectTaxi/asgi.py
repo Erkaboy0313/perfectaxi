d4c108ec74bbd106b0d07eac8d6293a8b60d8da9
Erkaboy0313 <fayyozbekov20@gmail.com>
2023-08-21 14:49:53 +0500

    va

--- PerfectTaxi/asgi.py
+++ PerfectTaxi/asgi.py
@@ -1,29 +1,25 @@
-"""
-ASGI config for channelsExample project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
-"""
 import os
 import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PerfectTaxi.settings')
+django.setup()
+
 from utils.wsauthentication import TokenAuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PerfectTaxi.settings')
-django.setup()
 django_asgi_app = get_asgi_application()
 
 import ws.routing
 
-application = ProtocolTypeRouter(
-    {
-        "http": django_asgi_app,
-        "websocket": AllowedHostsOriginValidator(
-            TokenAuthMiddlewareStack(URLRouter(ws.routing.websocket_urlpatterns))
+application = ProtocolTypeRouter({
+  "http": django_asgi_app,
+    "websocket":AllowedHostsOriginValidator(
+        TokenAuthMiddlewareStack(
+            URLRouter(
+                ws.routing.websocket_urlpatterns
+            )
         ),
-    }
-)
+    ),
+})
