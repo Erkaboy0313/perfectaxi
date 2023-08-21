601434499b0c5d0804170f6e4ec48cf0c58cc860
Erkaboy0313 <fayyozbekov20@gmail.com>
2023-08-21 14:45:32 +0500

    mistake

--- PerfectTaxi/asgi.py
+++ PerfectTaxi/asgi.py
@@ -7,14 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 import os
+import django
 from utils.wsauthentication import TokenAuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PerfectTaxi.settings')
-# Initialize Django ASGI application early to ensure the AppRegistry
-# is populated before importing code that may import ORM models.
+django.setup()
 django_asgi_app = get_asgi_application()
 
 import ws.routing
