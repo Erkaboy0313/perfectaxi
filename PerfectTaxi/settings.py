901f1688f84f1aae32f0099f8158412d2711a621
Erkaboy0313 <fayyozbekov20@gmail.com>
2023-09-19 12:23:11 +0500

    database

--- PerfectTaxi/settings.py
+++ PerfectTaxi/settings.py
@@ -133,9 +133,17 @@ CACHES = {
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'taxi',
+        'USER': 'taxiadmin',
+        'PASSWORD': 'taxiadmin',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
