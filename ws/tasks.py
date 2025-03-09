a947274eab1159ccd926b6ece19b50662af643e8
Erkaboy0313 <fayyozbekov20@gmail.com>
2025-03-09 16:21:10 +0500

    dd

--- ws/tasks.py
+++ ws/tasks.py
@@ -97,7 +97,10 @@ def find_drivers_to_order(order,location,service,order_time_str):
     
     extra_data = sgetKey(f'order_extra_info_{order_id}')
     Log.objects.create(text = f'{order_id} uchun driver qidirish taski')
+    
+    Log.objects.create(text = f'{order_id} uchun status {extra_data['status']}')
     if not extra_data['status'] == Order.OrderStatus.ACTIVE:
+        Log.objects.create(text = f'{order_id} bye bye')
         return
 
     if (order_time + timedelta(minutes=6)) < datetime.now(tz) :
