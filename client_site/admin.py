5892a2cbe08e8f72fb4eb9757e2e8ebf30bdbf3b
Erkaboy0313 <fayyozbekov20@gmail.com>
2025-03-23 15:37:42 +0500

    ss

--- client_site/admin.py
+++ client_site/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
-from .models import AppLinks,Service,Pricing,Statistic,Review,DriverRequest
+from .models import AppLinks,Service,Pricing,Statistic,Review,DriverRequest,ContactUs
 from parler.admin import TranslatableAdmin
 
 
 @admin.register(Service)
 class ServiceAdmin(TranslatableAdmin):
     list_display = ("name","title")
+
+@admin.register(ContactUs)
+class ContactUsAdmin(TranslatableAdmin):
+    list_display = ("phone","email")
 
 @admin.register(Pricing)
 class PricingAdmin(TranslatableAdmin):
